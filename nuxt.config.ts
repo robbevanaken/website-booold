86217826567d09f277575b529db49b8c8cbf0ea7
Robbe <robbe@weareantenna.be>
2025-09-12 17:42:56 +0200

    chore: setup

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  css: ['@@/assets/css/main.css'],
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/scripts'
+  ]
+})
