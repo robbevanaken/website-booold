07cc73395f2201ba7fa69bf8bd3baf025de45534
Robbe <robbe@weareantenna.be>
2025-10-26 20:31:06 +0100

    chore: footer bottom

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -1,39 +1,55 @@
 <template>
     <footer class="c-footer">
-        <div class="o-container o-grid">
-            <div class="col-span-12 md:col-span-6">
-                <Label
-                    text="Contact"
-                    classes="c-label--light"
-                ></Label>
-                <ul class="c-footer__list">
-                    <li class="c-footer__list-item">
-                        <a href="mailto:hello@boooldstudio.com" target="_blank" aria-label="Booold Email"><span data-button-animate-chars="" >hello@boooldstudio.com</span></a>
-                    </li>
-                    <li class="c-footer__list-item">
-                        <a href="tel:+32 494 48 77 62" target="_blank" aria-label="Booold Tel"><span data-button-animate-chars="" >+32 494 48 77 62</span></a>
-                    </li>
-                </ul>
+        <div class="c-footer__top">
+            <div class="o-container o-grid">
+                <div class="col-span-12 md:col-span-6">
+                    <Label
+                        text="Contact"
+                        classes="c-label--light"
+                    ></Label>
+                    <ul class="c-footer__list">
+                        <li class="c-footer__list-item">
+                            <a href="mailto:hello@boooldstudio.com" target="_blank" aria-label="Booold Email"><span data-button-animate-chars="" >hello@boooldstudio.com</span></a>
+                        </li>
+                        <li class="c-footer__list-item">
+                            <a href="tel:+32 494 48 77 62" target="_blank" aria-label="Booold Tel"><span data-button-animate-chars="" >+32 494 48 77 62</span></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-span-12 md:col-span-6">
+                    <Label
+                        text="Socials"
+                        classes="c-label--light"
+                    ></Label>
+                    <ul class="c-footer__list">
+                        <li class="c-footer__list-item">
+                            <a href="https://www.linkedin.com/company/booold/" target="_blank" aria-label="Booold Instagram"><span data-button-animate-chars="" >Linkedin</span></a>
+                        </li>
+                        <li class="c-footer__list-item">
+                            <a href="instagram.com" target="_blank" aria-label="Booold Instagram"><span data-button-animate-chars="" >Instagram</span></a>
+                        </li>
+                        <li class="c-footer__list-item">
+                            <a href="https://open.spotify.com/playlist/65vC87oYNWfBAg7MIE1ZZt?si=934630d01d7f4dfc" target="_blank" aria-label="Booold Spotify"><span data-button-animate-chars="" >Spotify</span></a>
+                        </li>
+                        <li class="c-footer__list-item">
+                            <a href="whatsapp.com" target="_blank" aria-label="Booold Whatsapp"><span data-button-animate-chars="" >Whatsapp</span></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="col-span-12 md:col-span-6">
-                <Label
-                    text="Socials"
-                    classes="c-label--light"
-                ></Label>
-                <ul class="c-footer__list">
-                    <li class="c-footer__list-item">
-                        <a href="https://www.linkedin.com/company/booold/" target="_blank" aria-label="Booold Instagram"><span data-button-animate-chars="" >Linkedin</span></a>
-                    </li>
-                    <li class="c-footer__list-item">
-                        <a href="instagram.com" target="_blank" aria-label="Booold Instagram"><span data-button-animate-chars="" >Instagram</span></a>
-                    </li>
-                    <li class="c-footer__list-item">
-                        <a href="https://open.spotify.com/playlist/65vC87oYNWfBAg7MIE1ZZt?si=934630d01d7f4dfc" target="_blank" aria-label="Booold Spotify"><span data-button-animate-chars="" >Spotify</span></a>
-                    </li>
-                    <li class="c-footer__list-item">
-                        <a href="whatsapp.com" target="_blank" aria-label="Booold Whatsapp"><span data-button-animate-chars="" >Whatsapp</span></a>
-                    </li>
-                </ul>
+        </div>
+        <div class="c-footer__bottom">
+            <div class="o-container o-grid">
+                <div class="col-span-12">
+                    <ul class="c-footer__list--sm">
+                        <li class="c-footer__list-item--sm">
+                            <a href=""><span data-button-animate-chars="" >Terms & Conditions</span></a>
+                        </li>
+                        <li class="c-footer__list-item--sm">
+                            <a href=""><span data-button-animate-chars="" >Privacy Policy</span></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </footer>
