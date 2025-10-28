6425612f2efebaf2a5095b0a3107ffb86d97c7be
Robbe <robbe@weareantenna.be>
2025-10-28 18:37:22 +0100

    chore: toggle menu

--- app/components/ButtonMenu.vue
+++ app/components/ButtonMenu.vue
@@ -1,18 +1,21 @@
 <template>
-  <button 
-    class="btn-menu" 
-    :aria-expanded="isExpanded" 
-    aria-label="Menu"
-    @click="toggleMenu"
-  >
-    <div class="btn-menu__bg"></div>
-    <div class="btn-menu__icon">
-      <span></span>
-      <span></span>
-    </div>
-  </button>
-  <aside class="c-slideout" :aria-hidden="!isExpanded">
-  </aside>
+  <div class="btn-menu__wrapper">
+    <button 
+      class="btn-menu" 
+      :aria-expanded="isExpanded" 
+      aria-label="Menu"
+      @click="toggleMenu"
+    >
+      <div class="btn-menu__bg"></div>
+      <div class="btn-menu__icon">
+        <span></span>
+        <span></span>
+      </div>
+    </button>
+    <aside class="c-slideout" :aria-hidden="!isExpanded">
+
+    </aside>
+  </div>
 </template>
 
 <script setup>
