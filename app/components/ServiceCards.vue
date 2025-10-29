98c47695a4f3f608d729501242f2804dd69c22f5
Robbe <robbe@weareantenna.be>
2025-10-29 18:39:58 +0100

    chore: tweaks

--- app/components/ServiceCards.vue
+++ app/components/ServiceCards.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="c-service-cards">
         <div class="o-container o-grid">
-      <div 
-            v-for="(service, index) in services" 
-            :key="service.id || index"
-            class="col-span-12 md:col-span-6 lg:col-span-3 c-service-cards__card-col"
-            >
-            <div class="c-service-cards__card">
-                <Label
-                    :text="index"
-                    classes="c-label--light mb-4">
-                </Label>
-                    <div>
-                        <h3 class="c-service-cards__card-title">{{ service.title }}</h3>
-                        <div class="c-service-cards__card-text">
-                        <p>{{ service.description }}</p>
+            <Label
+                text="Our services"
+                classes="c-label--dark mb-10 col-span-12 mx-auto">
+            </Label>
+            <div 
+                v-for="(service, index) in services" 
+                :key="service.id || index"
+                class="col-span-12 md:col-span-6 lg:col-span-3 c-service-cards__card-col"
+                >
+                <div class="c-service-cards__card">
+                        <Label
+                            :text="index"
+                            classes="c-label--light mb-4">
+                        </Label>
+                        <div>
+                            <h3 class="c-service-cards__card-title">{{ service.title }}</h3>
+                            <div class="c-service-cards__card-text">
+                            <p>{{ service.description }}</p>
                         </div>
                     </div>
                 </div>
