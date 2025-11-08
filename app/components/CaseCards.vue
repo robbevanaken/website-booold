b03fced0abb9220d08a12f01d11d989c8ccfa7fa
Robbe <robbe@weareantenna.be>
2025-11-08 16:09:38 +0100

    chore: cases cards offset

--- app/components/CaseCards.vue
+++ app/components/CaseCards.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="c-case-cards" id="work">
-        <div 
-            v-for="(casePost, index) in cases" 
-            :key="casePost.id || index"
-            class="c-case-cards__thumbnail">
-            
-            <h2>{{  casePost.title  }}</h2>
+        <div class="o-container o-grid">
+            <div 
+                v-for="(casePost, index) in cases" 
+                :key="casePost.id || index"
+                :class="'c-case-cards__thumbnail col-span-12 md:col-span-6 lg:col-span-5 ' + (index % 2 === 0 ? 'lg:col-start-1' : 'lg:col-start-8 md:mt-40 lg:mt-64')">
+                
+                <h2>{{  casePost.title  }}</h2>
+            </div>
         </div>
     </div>
 </template>
